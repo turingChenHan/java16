4540f0ffde48d1f0dea98874abf82a898091af2b
fred <fred@163.com>
2019-12-19 11:32:15 +0800

    添加项目

---
+++ springboot03/src/main/java/com/turing/springboot/Springboot03Application.java
@@ -0,0 +1,14 @@
+package com.turing.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+
+@SpringBootConfiguration
+public class Springboot03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03Application.class, args);
+    }
+
+}
